8be277ab08530e1b6d64624bfc5bbafc8d40da88
meanieminnie <mrinalini.nayak12@journalism.cuny.edu>
2023-05-02 20:29:33 -0400

    wrote js code for x & y axes

---
+++ script.js
@@ -0,0 +1,44 @@
+// SVG+D3 margin convention
+const margin = {top: 20, right: 30, bottom: 40, left: 100},
+    width = 900 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+// boilerplate for setting up the SVG
+let svg = d3.select("#dataviz")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+
+// ScaleLinear
+const xScale = d3.scaleTime()
+.domain([new Date(2019,00) , new Date(2023,00)]) // input data
+.range([0, width]) // output visual representation
+
+
+
+// const circleSizeScale = d3.scaleLinear()
+// .domain([0 , 1000000]) // input data
+// .range([1, 25]) //radius
+
+
+// scaleBand
+const yScale = d3.scaleLinear()
+.domain([0, 200000])
+.range([height , 0])
+
+
+
+
+// use d3 to draw axes based on the scales
+svg.append("g").call(d3.axisLeft(yScale))
+
+svg.append("g")
+.attr("transform", `translate(0, ${height})`)
+.call(d3.axisBottom(xScale))
+
+// svg.append("g").call(d3.axisBottom(xScale))
+
